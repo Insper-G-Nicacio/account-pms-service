70f35bb98d3efa6dc357bebc34af2b33e8e7f8ee
Asthare1709 <gustavo.aydelkop@gmail.com>
2026-02-27 13:36:17 -0300

    repository

--- src/main/java/insper/store/account/Account.java
+++ src/main/java/insper/store/account/Account.java
@@ -1,5 +1,16 @@
 package insper.store.account;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data @Builder
+@Accessors(fluent = true)
 public class Account {
-    
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
 }
